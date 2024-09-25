4e89029dd226c5c5f68df07a673de07cb3d76f88
Snake Man <snakeman@phnet.fi>
2024-09-25 11:06:07 +0000

    my first commit, yippy

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Linux PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Linux PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+<h1>Linux PMC Tactical</h1>
+<p>Welcome to Linux PMC Tactical.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="links.php">Links</a>, back to <a href="index.php">Linux PMC Tactical root page</a></p>
+<br><br>
+<p><i>PMC Linux PMC Tactical 1992 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Links Linux PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Links Linux PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+<h1>Links Linux PMC Tactical</h1>
+<p>Useful web links Linux PMC Tactical.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+
+<?php include("/var/www/include/links.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>Back to <a href="index.php">Linux PMC Tactical root page</a></p>
+<br><br>
+<p><i>PMC Linux PMC Tactical 1992 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
