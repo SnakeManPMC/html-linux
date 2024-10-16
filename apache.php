47393a2fb3c312f209ee025f976edd4070b78e20
Snake Man <snakeman@phnet.fi>
2024-10-16 00:28:22 +0000

    apache page created

--- apache.php
+++ apache.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Linux PMC Tactical</title>
+<title>Apache Web Server Linux PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="Linux PMC Tactical">
+<META name="description" content="Apache Web Server Linux PMC Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,18 +15,51 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1>Linux PMC Tactical</h1>
-<p>Welcome to Linux PMC Tactical.</p>
+	<h1>Apache Web Server Linux PMC Tactical</h1>
+<p>Welcome to Apache Web Server Linux PMC Tactical.</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
 <?php include("/var/www/include/support.php"); ?>
-	<h2>Welcome To My Linux World</h2>
+	<h2>Apache Web Server</h2>
 
-<p><a href="about.php">About</a> quick description what this website is all about.</p>
-<p><a href="apache.php">Apache</a> web server.</p>
+<p>
+Apache is a very well known common web server software for Linux and other operating systems. See <a href="https://httpd.apache.org/" target="_blank">httpd.apache.org</a> website for details.
+</p>
+
+Apache enable rewrite mod:
+<pre class="bash">
+sudo a2enmod rewrite
+</pre>
+
+Apache enable ssl mod:
+<pre class="bash">
+a2enmod ssl
+</pre>
+
+Web site enabling:
+<pre class="bash">
+sudo a2ensite your_domain_1.conf
+sudo a2ensite your_domain_2.conf
+</pre>
+If you need to disable site, use ''a2dissite'' command.
+
+Apache config file syntax test:
+<pre class="bash">
+sudo apache2ctl configtest
+</pre>
+
+Apache status:
+<pre class="bash">
+sudo systemctl status apache2
+</pre>
+
+Apache restart:
+<pre class="bash">
+sudo systemctl restart apache2
+</pre>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
